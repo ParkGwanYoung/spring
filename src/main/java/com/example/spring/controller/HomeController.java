d5fdd0b141316d495dedb32c8a3ccca6cd92918b
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-20 11:46:34 +0900

    park

--- src/main/java/com/example/spring/controller/HomeController.java
+++ src/main/java/com/example/spring/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/home")
-    public String action(){
+    public String action() {
         return "home";
     }
 }
