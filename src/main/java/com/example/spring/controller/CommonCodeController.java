70d196665535f4df3674075e8e591dfc56bc314c
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-20 17:32:41 +0900

    park

--- src/main/java/com/example/spring/controller/CommonCodeController.java
+++ src/main/java/com/example/spring/controller/CommonCodeController.java
@@ -1,12 +1,36 @@
 package com.example.spring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/commonCode")
-public class CommonCodeController {
-    @RequestMapping(value = "/edit",method = {RequestMethod.GET,RequestMethod.POST})
-    public void edit(){}
+
+public class CommonCodeController{
+    
+    @RequestMapping(value = "/commonCode/{action}")
+    public ModelAndView edit(@RequestParam Map<String, Object> paramMap,@PathVariable String action, ModelAndView modelandView){
+        //위에 --서버가 클라이언트로 보내는 문법
+        //아래 서버가 클라이언트로 응답하기 위한 문법
+        String viewName = "/commonCode/";
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        if("edit".equals(action)){
+            viewName = viewName + action;
+            resultMap = paramMap;
+        }
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultMap", resultMap);
+        return modelandView;
+
+    }
+
+    
+    @RequestMapping(value = "/commonCode/insert")
+    public void actionmethod(){
+    }
 }
