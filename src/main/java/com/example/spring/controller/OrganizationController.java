908ae7ee39e29f5da480ea35d387bd9a15edbf31
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-20 16:35:02 +0900

    park

--- src/main/java/com/example/spring/controller/OrganizationController.java
+++ src/main/java/com/example/spring/controller/OrganizationController.java
@@ -8,13 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/read")
 
 public class OrganizationController{
+    
+    @RequestMapping(value = "/read")
     public ModelAndView read(@RequestParam Map<String, Object> paramMap, ModelAndView modelandView){
+        //위에 --서버가 클라이언트로 보내는 문법
+        //아래 서버가 클라이언트로 응답하기 위한 문법
         String viewName = "/organization/read";
         modelandView.setViewName(viewName);
-        modelandView.addObject("paramMap", paramMap);
+        modelandView.addObject("resultMap", paramMap);
         return modelandView;
+
+    }
+
+    
+    @RequestMapping(value = "/organization/insert")
+    public void actionmethod(){
     }
 }
