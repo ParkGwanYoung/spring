f5ee4d9b3ecc90b07be9942391329f92ec3951a2
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-21 17:50:32 +0900

    park

--- src/main/java/com/example/spring/controller/MemberController2.java
+++ src/main/java/com/example/spring/controller/MemberController2.java
@@ -6,11 +6,13 @@ import java.util.Map;
 import com.example.spring.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class MemberController2 {
 
     @Autowired
