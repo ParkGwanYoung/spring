2cfa8ab558207cce887078a1b8f98efef4ac8ba0
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-22 10:27:46 +0900

    park

--- src/main/java/com/example/spring/controller/MemberDaoController.java
+++ src/main/java/com/example/spring/controller/MemberDaoController.java
@@ -1,20 +1,33 @@
+package com.example.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.example.spring.service.MemberDaoService;
+import com.example.spring.service.MemberService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MemberDaoController {
 
     @Autowired
-    private MemberService service;
+    private MemberDaoService service;
 
     @RequestMapping(value = "/dao/{action}")
-    public ModelAndView List(@RequestParam Map<String, Object> paramMap,@PathVariable String action, ModelAndView modelandView){
-        
-        java.util.List<Object> resultList = new ArrayList<Object>();
+    public ModelAndView list(@RequestParam Map<String, Object> paramMap,@PathVariable String action, ModelAndView modelandView){
+
         String viewName = "/dao/";
-        
+        List<Object> resultList = new ArrayList<Object>();
        
         if("read".equals(action)){
             viewName = viewName + action;
-            resultList = (List<Object>) service.getObject(paramMap);
+            resultList = (List<Object>)service.getObject(paramMap);
         }
         modelandView.setViewName(viewName);
         modelandView.addObject("resultList", resultList);
