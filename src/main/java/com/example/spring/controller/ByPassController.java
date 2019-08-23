1ec56e1fd8865f1af5ce92b0aa42e159c085229f
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-23 11:02:59 +0900

    park

--- src/main/java/com/example/spring/controller/ByPassController.java
+++ src/main/java/com/example/spring/controller/ByPassController.java
@@ -1,9 +1,11 @@
 package com.example.spring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class ByPassController {
-    @RequestMapping(value = { "/thymeleafmerge/thymeleafBase" })
-    public void bypass() {
+    @RequestMapping(value = { "/layout/defaultLayout" })
+    public void byPass() {
     }
 }
