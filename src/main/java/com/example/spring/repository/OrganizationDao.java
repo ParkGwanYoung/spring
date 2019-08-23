1e8bdf5a69a57dafa1ef3b6afe845739cd381854
ParkGwanYoung <rhksdyd846@gmail.com>
2019-08-23 09:16:25 +0900

    park

---
+++ src/main/java/com/example/spring/repository/OrganizationDao.java
@@ -0,0 +1,15 @@
+package com.example.spring.repository;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrganizationDao{
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+    public Object getList(String sqlMapId, Object dataMap){
+        Object result = sqlSession.selectList(sqlMapId,dataMap);
+        return result;
+    }
+}
